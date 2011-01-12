608cdd006a625d8a5c304fb0a2400a5bc6f68ea4
opsb <oliver@opsb.co.uk>
2011-01-12 00:12:44 +0000

    asdfafd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testrb::Application.config.secret_token = '0040fc7f6e7b905537c2e3b528b626af1a4d35392eb9ba4927dbf34229c7f1fc4f7d104f8e2d38def531479194557e112794fb96c51338a098dbb6855c3129bd'
